bb9601610a978255936414561b8169451116de9d
yunsjak <yunsujung0722@gmail.com>
2025-04-24 11:39:29 +0900

    실습내용

--- src/com/woori/BAM/GradeCalc.java
+++ src/com/woori/BAM/GradeCalc.java
@@ -10,19 +10,20 @@ public class GradeCalc {
 
 		System.out.print("총 학생 수 : "); // 총 몇 명의 학생인지 입력
 		student_count = sc.nextInt();
+		sc.nextLine();
 
 		String[] names = new String[student_count];
 		int[] scores = new int[student_count];
 		String[] grades = new String[student_count];
 
 		for (int i = 0; i < student_count; i++) { // 입력 받은 학생 수 만큼 반복하여 학생 이름과 점수 입력 받음
+			System.out.printf("%d번째 학생 이름 : ", i + 1);
+			names[i] = sc.nextLine();
+			
 			System.out.printf("%d번째 학생 점수 : ", i + 1);
 			scores[i] = sc.nextInt();
 			sc.nextLine();
 
-			System.out.printf("%d번째 학생 이름 : ", i + 1); 
-			names[i] = sc.nextLine();
-			
 
 			switch (scores[i] / 10) { // 점수를 기준으로 등급 판단
 			case 10:
@@ -37,6 +38,7 @@ public class GradeCalc {
 			}
 		}
 		// 모든 학생의 이름, 점수, 등급을 한 번에 출력
+		System.out.println();
 		System.out.printf("%d 명 학생의 점수 등급 \n", student_count);
 		System.out.println("===========================================");
 		for (int i = 0; i < student_count; i++) {
--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -1,5 +1,7 @@
 package com.woori.BAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,22 +10,24 @@ public class Main {
 		Scanner sc = new Scanner(System.in); // 최적화 => 자원 1번 사용
 
 		int id = 1;
+		List<Article> articles = new ArrayList<>();
 
 		while (true) {
 			// System.out.printf("commend : ");
 			// String cmd = sc.nextLine(); // cmd 변수 => 재사용
 			// System.out.println("answer : "+ cmd);
-			System.out.printf("commend : ");
+			System.out.printf("command : ");
 			String cmd = sc.nextLine().trim();
 
 			if (cmd.equals("exit")) {
 				break;
 			}
 			if (cmd.length() == 0) {
-				System.out.println("명령어를 입력해 주세요 : ");
+				System.out.println("명령어를 입력해 주세요");
 				continue;
 			}
 			if (cmd.equals("article list")) {
+
 				System.out.println("게시글이 없습니다");
 			} else if (cmd.equals("article write")) {
 
@@ -31,8 +35,30 @@ public class Main {
 				String title = sc.nextLine().trim();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine().trim();
-				System.out.println(id + "번글이 생성되었습니다");
+
+				Article article = new Article(id,title,body); 
+				// Article article = new Article(); 		  // article은 지역 변수
+															  // 따라서 저장된 것은 아님
+				// 최적화 작업 : 생성자 이용해서 초기화
+//				article.id = id;							  					
+//				article.title = title;											  
+//				article.body = body;															
+				
+				// 진짜 저장은 아래 문장을 통해서 진행
+				articles.add(article); // List 구조인 ArrayList 객체인 articles 저장
+				
+				// 최적화2 - 1줄로 위에 두 줄 기능 실행
+//				articles.add(new Article(id,title,body));
+
+				System.out.println(id + "번 글이 생성되었습니다");
 				id++;
+
+			} else if (cmd.equals("test")) {
+				for (int i = 0; i < articles.size(); i++) {
+					System.out.println(articles.get(i).id);
+					System.out.println(articles.get(i).title);
+					System.out.println(articles.get(i).body);
+				}
 
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다");
@@ -41,4 +67,17 @@ public class Main {
 		sc.close();
 		System.out.println("== 프로그램 종료 ==");
 	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+	
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+
 }
