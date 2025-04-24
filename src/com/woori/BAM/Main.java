46c9d2876426743f35aa41870782638e0edc9475
yunsjak <yunsujung0722@gmail.com>
2025-04-24 14:55:41 +0900

    List / ArrayList - size(),get() 메서드활용 실습

--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -13,9 +13,6 @@ public class Main {
 		List<Article> articles = new ArrayList<>();
 
 		while (true) {
-			// System.out.printf("commend : ");
-			// String cmd = sc.nextLine(); // cmd 변수 => 재사용
-			// System.out.println("answer : "+ cmd);
 			System.out.printf("command : ");
 			String cmd = sc.nextLine().trim();
 
@@ -26,9 +23,19 @@ public class Main {
 				System.out.println("명령어를 입력해 주세요");
 				continue;
 			}
+			// List 메서드 중 size() 이용 (Data 유무를 객체의 개수(크기)로 변환)
 			if (cmd.equals("article list")) {
-
-				System.out.println("게시글이 없습니다");
+				if (articles.isEmpty()) {
+					System.out.println("게시글이 존재하지 않습니다.");
+					continue;
+				}
+					// 배열 사용해서 get() 사용 ==> 객체를 리턴 받음
+					System.out.println("번호 | 제목  | 내용");
+					for (int i = articles.size() - 1; i >= 0; i--) {
+						Article article = articles.get(i); // article 재사용 목적 : articles.get(i)의 기능을 저장할 변수
+						System.out.printf(" %d   |  %s  |  %s \n", article.id, article.title,article.body);
+					}
+					
 			} else if (cmd.equals("article write")) {
 
 				System.out.printf("제목 : ");
@@ -36,29 +43,13 @@ public class Main {
 				System.out.printf("내용 : ");
 				String body = sc.nextLine().trim();
 
-				Article article = new Article(id,title,body); 
-				// Article article = new Article(); 		  // article은 지역 변수
-															  // 따라서 저장된 것은 아님
-				// 최적화 작업 : 생성자 이용해서 초기화
-//				article.id = id;							  					
-//				article.title = title;											  
-//				article.body = body;															
-				
+				Article article = new Article(id, title, body);
+
 				// 진짜 저장은 아래 문장을 통해서 진행
 				articles.add(article); // List 구조인 ArrayList 객체인 articles 저장
-				
-				// 최적화2 - 1줄로 위에 두 줄 기능 실행
-//				articles.add(new Article(id,title,body));
 
 				System.out.println(id + "번 글이 생성되었습니다");
 				id++;
-
-			} else if (cmd.equals("test")) {
-				for (int i = 0; i < articles.size(); i++) {
-					System.out.println(articles.get(i).id);
-					System.out.println(articles.get(i).title);
-					System.out.println(articles.get(i).body);
-				}
 
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다");
@@ -73,7 +64,7 @@ class Article {
 	int id;
 	String title;
 	String body;
-	
+
 	public Article(int id, String title, String body) {
 		this.id = id;
 		this.title = title;
