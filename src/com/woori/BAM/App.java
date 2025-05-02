5de529458458ccc44bdb43da2d80791dffa5227c
yunsjak <yunsujung0819@naver.com>
2025-05-02 16:37:06 +0900

    리펙토링(최적화) 접근제어자/ 중복 소스 메서드화 2개 추가

--- src/com/woori/BAM/App.java
+++ src/com/woori/BAM/App.java
@@ -8,14 +8,13 @@ import com.woori.BAM.dto.Article;
 import com.woori.BAM.util.Util;
 
 public class App {
-	  // 전역변수 ==> this (자기 자신을 의미, 객체)
-	  List<Article> articles ;   // List 타입의 articles
-	  int lastArticleId ;
-	  
-	 // 생성자를 통해서 초기화.  기존 code (static 제거 한 상태)
+	
+	private List<Article> articles ;   
+	private  int no ;
+	 
 	 App() {
-		 articles = new ArrayList<>(); // 데이터의 구조 ArrayList 형태 객체가 생성
-		 lastArticleId = 1;
+		 articles = new ArrayList<>();
+		 no = 1;
 	 }
 	
 	void run() {
@@ -43,83 +42,64 @@ public class App {
 				System.out.printf("내용 : ");
 				String body = sc.nextLine().trim();
 
-				Article article = new Article(lastArticleId, Util.getDateStr(), title, body, 0);
+				Article article = new Article(no, Util.getDateStr(), title, body, 0);
 				articles.add(article);
 
-				System.out.println(lastArticleId + "번 글이 생성되었습니다");
-				lastArticleId++;
+				System.out.println(no + "번 글이 생성되었습니다");
+				no++;
 
 			} else if (cmd.equals("article list")) {
 				if (articles.size() == 0) {
 					System.out.println("존재하는 게시글이 없습니다");
 					continue;
 				}
-
+				
 				// 타이틀 줄 수정 (내용을 제거)
-				System.out.println("번호	|	제목	|		날짜		|	조회수");
+				System.out.println("번호  |  제목   |  내용   |  날짜                   | 조회수");
 
 				// 출력 수정(상동)
 				for (int i = articles.size() - 1; i >= 0; i--) {
-					Article article = articles.get(i);
-					System.out.printf("%d	|	%s	|	%s	|	%d\n", article.getId(), article.getTitle(), article.getRegDate(), article.getViewCnt());
+					Article article = articles.get(i); // article 재사용 목적 : articles.get(i)의 기능을 저장할 변수
+					System.out.printf(" %d    |  %s  |  %s  |   %s   |  %d \n", article.getId(), article.getTitle(),
+							article.getBody(), article.getRegDate(), article.getViewCnt());
 				}
 				
 			} else if (cmd.startsWith("article detail ")) {
-				String[] cmdBits = cmd.split(" ");
-
-				int id = 0;
 				
-				try {
-					id = Integer.parseInt(cmdBits[2]);
-				} catch (NumberFormatException e) {
-					System.out.println("명령어가 올바르지 않습니다");
+				int id = getCmdId(cmd);   // id or 0 
+				
+				// id 가 0으로 리턴시 처리 로직 보강
+				if ( id == 0) {
 					continue;
 				}
 				
-				Article foundArticle = null;
+				Article check = getArticleById(id);  // article or null
 				
-				for (Article article : articles) {
-					if (article.getId() == id) {
-						foundArticle = article;
-						break;
-					}
-				}
-				
-				if (foundArticle == null) {
+				if (check == null) {
 					System.out.println(id + "번 게시물이 존재하지 않습니다");
 					continue;
 				}
 				
-				foundArticle.increaseViewCnt();
+				check.increaseViewCnt();
 				
-				System.out.println("번호 : " + foundArticle.getId());
-				System.out.println("날짜 : " + foundArticle.getRegDate());
-				System.out.println("제목 : " + foundArticle.getTitle());
-				System.out.println("내용 : " + foundArticle.getBody());
-				System.out.println("조회수 : " + foundArticle.getViewCnt());
+				System.out.println("번호 : " + check.getId());
+				System.out.println("날짜 : " + check.getRegDate());
+				System.out.println("제목 : " + check.getTitle());
+				System.out.println("내용 : " + check.getBody());
+				System.out.println("조회수 : " + check.getViewCnt());
 				
 			} else if (cmd.startsWith("article modify ")) {
-				String[] cmdBits = cmd.split(" ");
 
-				int id = 0;
+				int id = getCmdId(cmd);   // id or 0 
 				
-				try {
-					id = Integer.parseInt(cmdBits[2]);
-				} catch (NumberFormatException e) {
-					System.out.println("명령어가 올바르지 않습니다");
+				// id 가 0으로 리턴시 처리 로직 보강
+				if ( id == 0) {
 					continue;
 				}
 				
-				Article foundArticle = null;
+				Article check = getArticleById(id);  // article or null
 				
-				for (Article article : articles) {
-					if (article.getId() == id) {
-						foundArticle = article;
-						break;
-					}
-				}
-				
-				if (foundArticle == null) {
+				if (check == null) {
 					System.out.println(id + "번 게시물이 존재하지 않습니다");
 					continue;
 				}
@@ -129,38 +109,28 @@ public class App {
 				System.out.printf("수정할 내용 : ");
 				String body = sc.nextLine().trim();
 				
-				foundArticle.setTitle(title);
-				foundArticle.setBody(body);
+				check.setTitle(title);
+				check.setBody(body);
 			
 				System.out.println(id + "번 게시물이 수정되었습니다");
 				
 			} else if (cmd.startsWith("article delete ")) {
-				String[] cmdBits = cmd.split(" ");
-
-				int id = 0;
 				
-				try {
-					id = Integer.parseInt(cmdBits[2]);
-				} catch (NumberFormatException e) {
-					System.out.println("명령어가 올바르지 않습니다");
+				int id = getCmdId(cmd);   // id or 0 
+				
+				// id 가 0으로 리턴시 처리 로직 보강
+				if ( id == 0) {
 					continue;
 				}
 				
-				Article foundArticle = null;
+				Article check = getArticleById(id);  // article or null
 				
-				for (Article article : articles) {
-					if (article.getId() == id) {
-						foundArticle = article;
-						break;
-					}
-				}
-				
-				if (foundArticle == null) {
+				if (check == null) {
 					System.out.println(id + "번 게시물이 존재하지 않습니다");
 					continue;
 				}
 				
-				articles.remove(foundArticle);
+				articles.remove(check);
 				
 				System.out.println(id + "번 게시물이 삭제되었습니다");
 				
@@ -175,10 +145,41 @@ public class App {
 		System.out.println("== 프로그램 끝 ==");
 	}
 
+
+
+	private Article getArticleById(int id) {
+
+	    for (Article article : articles) {
+	        if (article.getId() == id) {
+		        return article;    // return article , 메서드 종료
+	        }
+	    }
+		
+		return null;
+	}
+
+	private int getCmdId(String cmd) {
+		
+		String[] cmdBits = cmd.split(" ");
+
+		int id = 0;
+		
+		try {
+			id = Integer.parseInt(cmdBits[2]);
+			return id;    // id 를 넘겨주고 메서드 종료
+			
+		} catch (NumberFormatException e) {
+			System.out.println("명령어가 올바르지 않습니다");
+			return 0;   // "asd" ---> 0 
+		}
+		
+		
+	}
+
 	private  void makeTestData() {
 		
 		for(int i = 1; i <= 5; i++) {
-    		articles.add(new Article(lastArticleId++, Util.getDateStr(), "제목" + i, "내용" + i, i * 10));
+    		articles.add(new Article(no++, Util.getDateStr(), "제목" + i, "내용" + i, i * 10));
 		}
 	}
 	
--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -1,10 +1,10 @@
 package com.woori.BAM;
 
 public class Main {
-	
-	 public static void main(String[] args) {
 
-		 new App().run();  // temp 성격 (일회용 객체를 만들고 run() 실행
+	public static void main(String[] args) {
 
+		new App().run(); // temp 성격 (일회용 객체를 만들고 run() 실행)
 	}
+
 }
